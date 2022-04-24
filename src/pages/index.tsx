d1bc59991df2c921125d437e7f2a926a2a124ab9
flavio silva <flavio.devjs@gmail.com>
2022-04-24 08:51:20 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Inicio | ig.news</title>
+            </Head>
+            <div>
+                
+            </div>
+        </>
+    )
+}
