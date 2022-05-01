d729132e5638b833cfa91b10a3c4d0687c0734e7
flavio silva <flavio.devjs@gmail.com>
2022-05-01 09:57:30 -0300

    implemented hero on home page and your styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,26 @@
-import Head from "next/head"
+import Head from "next/head";
+import Image from "next/image";
+
+import styles from './home.module.scss';
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Inicio | ig.news</title>
-            </Head>
-            <div>
-                
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>Inicio | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width={600} height={600} />
+      </main>
+    </>
+  );
 }
