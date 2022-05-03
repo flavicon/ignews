d069634d88c96bda0d0ee41a073f620fa35b194a
flavio silva <flavio.devjs@gmail.com>
2022-05-03 08:49:58 -0300

    implemented redirect to stripe payment

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,13 +10,25 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
     const {data: session} = useSession();
 
-    function handleSubscribe() {
+    async function handleSubscribe() {
         if (!session) {
             signIn();
             return;
         }
 
-        //create a checkout
+        try {
+            const response = await api.post('/subscribe');
+
+            const { sessionId } = response.data;
+
+            console.log(sessionId);
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({ sessionId });
+        } catch (err) {
+            alert(err);
+        }
     };
 
     return (
